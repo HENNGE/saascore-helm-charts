ded4e67fac3528a75040abbb56c0e1c367a608c3
Shogo Iwano <shiwano@gmail.com>
2021-11-03 17:35:57 +0900

    upgrade temporal

--- update.sh
+++ update.sh
@@ -6,10 +6,10 @@ main() {
 	rm -rf ./temporal
 	rm -f ./temporal-0.2.2.tgz
 
-	curl -L -O https://github.com/temporalio/helm-charts/archive/v1.7.0.zip
-	unzip v1.7.0.zip
-	rm v1.7.0.zip
-	mv helm-charts-1.7.0 temporal
+	curl -L -O https://github.com/temporalio/helm-charts/archive/v1.13.0.zip
+	unzip v1.13.0.zip
+	rm v1.13.0.zip
+	mv helm-charts-1.13.0 temporal
 
 	cd temporal
 	helm dependencies update
