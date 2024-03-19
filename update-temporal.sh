455c920841d8be474fd4d3ba6800c7aa3e459040
Muhammad Furqan Habibi <furqan.habibi@hennge.com>
2024-03-19 19:28:23 +0900

    Rename helm chart dirs

--- update-temporal.sh
+++ update-temporal.sh
@@ -8,12 +8,12 @@ main() {
 	curl -L -O https://github.com/temporalio/helm-charts/archive/temporal-${temporal_ver}.zip
 	unzip temporal-${temporal_ver}.zip
 	rm temporal-${temporal_ver}.zip
-	mv helm-charts-temporal-${temporal_ver} temporal_${temporal_ver}
+	mv helm-charts-temporal-${temporal_ver} temporal_helm_chart_${temporal_ver}
 
-	cd temporal_${temporal_ver}/charts/temporal
+	cd temporal_helm_chart_${temporal_ver}/charts/temporal
 	helm dependencies update
 	cd ../../..
-	helm package temporal_${temporal_ver}/charts/temporal
+	helm package temporal_helm_chart_${temporal_ver}/charts/temporal
 	helm repo index .
 }
 
