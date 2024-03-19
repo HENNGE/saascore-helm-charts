da6c020d7cac1e9fd3e3c41f0ecb0d686f9dd98f
Muhammad Furqan Habibi <furqan.habibi@hennge.com>
2024-03-19 19:03:04 +0900

    Handle new temporal helm chart release files

--- update-temporal.sh
+++ update-temporal.sh
@@ -3,17 +3,17 @@
 main() {
 	set -eu
 
-	temporal_ver=1.22.2
+	temporal_ver=0.33.0
 
-	curl -L -O https://github.com/temporalio/helm-charts/archive/v${temporal_ver}.zip
-	unzip v${temporal_ver}.zip
-	rm v${temporal_ver}.zip
-	mv helm-charts-${temporal_ver} temporal_${temporal_ver}
+	curl -L -O https://github.com/temporalio/helm-charts/archive/temporal-${temporal_ver}.zip
+	unzip temporal-${temporal_ver}.zip
+	rm temporal-${temporal_ver}.zip
+	mv helm-charts-temporal-${temporal_ver} temporal_${temporal_ver}
 
-	cd temporal_${temporal_ver}
+	cd temporal_${temporal_ver}/charts/temporal
 	helm dependencies update
-	cd ../
-	helm package temporal_${temporal_ver}
+	cd ../../..
+	helm package temporal_${temporal_ver}/charts/temporal
 	helm repo index .
 }
 
