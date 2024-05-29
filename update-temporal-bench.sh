7d03fa6b34ac86c2c50a719b5df58edbed0dcbee
Muhammad Furqan Habibi <furqan.habibi@hennge.com>
2024-05-29 15:12:56 +0900

    Update temporal-bench using latest

--- update-temporal-bench.sh
+++ update-temporal-bench.sh
@@ -6,11 +6,11 @@ main() {
 	rm -rf ./temporal-bench
 	rm -f ./temporal-bench-0.1.0.tgz
 
-	curl -L -O https://github.com/temporalio/maru/archive/refs/tags/v0.2.0.zip
-	unzip v0.2.0.zip
-	rm v0.2.0.zip
-	mv maru-0.2.0/helm-chart temporal-bench
-	rm -r maru-0.2.0
+	curl -L -O https://github.com/temporalio/maru/archive/refs/heads/master.zip
+	unzip master.zip
+	rm master.zip
+	mv maru-master/helm-chart temporal-bench
+	rm -r maru-master
 
 	cd temporal-bench
 	helm dependencies update
